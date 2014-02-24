85dbf9d860d3a95d48056bacdd6e7640c19e78c0
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-24 11:16:17 +0100

    rename app into portail

--- config/env.rb
+++ config/env.rb
@@ -6,4 +6,4 @@ APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 # Configurer ici le virtual path de l'application/
 # /!\ Il est obligatoire de mettre l'application sous un virtual path du fait de notre architecture HAProxy.
 # On définit ici un virtual path sans le slash final.
-APP_PATH = '/app'
+APP_PATH = '/portail'
