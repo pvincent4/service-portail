997b58c0235f567e12859587f081d8d2cc0855a7
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-24 12:06:44 +0100

    style

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -2,16 +2,18 @@
 
 // Declare app level module which depends on filters, and services
 angular.module( 'portailApp',
-		[ 'portailApp.controllers', 'ngRoute', 'services.constants'] )
+		[ 'portailApp.controllers',
+		  'ngRoute',
+		  'services.constants' ] )
     .config( ['$routeProvider', 'APPLICATION_PREFIX',
 	      function( $routeProvider, APPLICATION_PREFIX ) {
 		  $routeProvider.when( '/view1',
-				       { templateUrl: APPLICATION_PREFIX+'/views/partial1.html',
+				       { templateUrl: APPLICATION_PREFIX + '/views/partial1.html',
 					 controller: 'PortailCtrl1' } );
 		  $routeProvider.when( '/view2',
-				       { templateUrl: APPLICATION_PREFIX+'/views/partial2.html',
+				       { templateUrl: APPLICATION_PREFIX + '/views/partial2.html',
 					 controller: 'PortailCtrl2' } );
-		  $routeProvider.otherwise( {redirectTo: '/view1'} );
+		  $routeProvider.otherwise( { redirectTo: '/view1' } );
 	      } ] )
     .run( [ '$rootScope', '$location',
 	    function( $rootScope, $location ) {
