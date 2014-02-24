48df00b57307eb64d6ea9db38e5101d5db0ffdb7
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-24 12:34:51 +0100

    style

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -5,16 +5,16 @@ angular.module( 'portailApp',
 		[ 'portailApp.controllers',
 		  'ngRoute',
 		  'services.constants' ] )
-    .config( ['$routeProvider', 'APPLICATION_PREFIX',
-	      function( $routeProvider, APPLICATION_PREFIX ) {
-		  $routeProvider.when( '/view1',
-				       { templateUrl: APPLICATION_PREFIX + '/views/partial1.html',
-					 controller: 'PortailCtrl1' } );
-		  $routeProvider.when( '/view2',
-				       { templateUrl: APPLICATION_PREFIX + '/views/partial2.html',
-					 controller: 'PortailCtrl2' } );
-		  $routeProvider.otherwise( { redirectTo: '/view1' } );
-	      } ] )
+    .config( [ '$routeProvider', 'APPLICATION_PREFIX',
+	       function( $routeProvider, APPLICATION_PREFIX ) {
+		   $routeProvider.when( '/view1',
+					{ templateUrl: APPLICATION_PREFIX + '/views/partial1.html',
+					  controller: 'PortailCtrl1' } );
+		   $routeProvider.when( '/view2',
+					{ templateUrl: APPLICATION_PREFIX + '/views/partial2.html',
+					  controller: 'PortailCtrl2' } );
+		   $routeProvider.otherwise( { redirectTo: '/view1' } );
+	       } ] )
     .run( [ '$rootScope', '$location',
 	    function( $rootScope, $location ) {
 		$rootScope.$location = $location;
