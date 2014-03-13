d1bada9ca71bcd19dda550c6e367ccea1725205c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-13 17:03:44 +0100

    commit les bower_components && remplace angular-route par angular-ui-router

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1,21 +1,23 @@
 'use strict';
 
-
-
 // Declare app level module which depends on filters, and services
 angular.module( 'portailApp',
 		[ 'portailApp.controllers',
-		  'ngRoute',
+		  'ui.router',
 		  'services.constants' ] )
-    .config( [ '$routeProvider', 'APPLICATION_PREFIX',
-	       function( $routeProvider, APPLICATION_PREFIX ) {
-		   $routeProvider.when( '/view1',
-					{ templateUrl: APPLICATION_PREFIX + '/views/partials/partial1.html',
-					  controller: 'PortailCtrl1' } );
-		   $routeProvider.when( '/view2',
-					{ templateUrl: APPLICATION_PREFIX + '/views/partials/partial2.html',
-					  controller: 'PortailCtrl2' } );
-		   $routeProvider.otherwise( { redirectTo: '/view1' } );
+    .config( [ '$stateProvider', '$urlRouterProvider', 'APPLICATION_PREFIX',
+	       function( $stateProvider, $urlRouterProvider, APPLICATION_PREFIX ) {
+		   $stateProvider
+		       .state( 'view1',
+			       { url: '/view1',
+				 templateUrl: APPLICATION_PREFIX + '/views/partials/partial1.html',
+				 controller: 'PortailCtrl1' } )
+		       .state( 'view2',
+			       { url: '/view2',
+				 templateUrl: APPLICATION_PREFIX + '/views/partials/partial2.html',
+				 controller: 'PortailCtrl2' } );
+
+		   $urlRouterProvider.otherwise( '/view1' );
 	       } ] )
     .run( [ '$rootScope', '$location',
 	    function( $rootScope, $location ) {
@@ -24,16 +26,16 @@ angular.module( 'portailApp',
 	    } ] );
 
 $(document).ready( function() {
-  var client = new Faye.Client( '/portail/faye', {
-      timeout: 120
-      });
+    var client = new Faye.Client( '/portail/faye', {
+	timeout: 120
+    });
 
-  var subscription = client.subscribe('/foo', function(msg) {
-    console.log("message received on /foo : " + msg) ;
-    $.growl.notice({ duration: 6400, size: "large", title: "Hey there", message: "New message" });
-    $.growl({ message: msg.text });
-    // handle message
-  });
+    var subscription = client.subscribe('/foo', function(msg) {
+	console.log("message received on /foo : " + msg) ;
+	$.growl.notice({ duration: 6400, size: "large", title: "Hey there", message: "New message" });
+	$.growl({ message: msg.text });
+	// handle message
+    });
 
-  client.publish('/foo', {text: 'Hi there'});
+    client.publish('/foo', {text: 'Hi there'});
 });
