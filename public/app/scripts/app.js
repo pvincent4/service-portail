ba53e648efd3af3168848425ced01c1b3fe1c4dd
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-17 09:25:35 +0100

    réorganisation des vues angularjs

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -9,15 +9,15 @@ angular.module( 'portailApp',
 	       function( $stateProvider, $urlRouterProvider, APPLICATION_PREFIX ) {
 		   $stateProvider
 		       .state( 'view1',
-			       { url: '/view1',
-				 templateUrl: APPLICATION_PREFIX + '/views/partials/partial1.html',
+			       { url: '/mire',
+				 templateUrl: APPLICATION_PREFIX + '/views/mire.html',
 				 controller: 'PortailCtrl1' } )
 		       .state( 'view2',
-			       { url: '/view2',
-				 templateUrl: APPLICATION_PREFIX + '/views/partials/partial2.html',
+			       { url: '/show-app',
+				 templateUrl: APPLICATION_PREFIX + '/views/show-app.html',
 				 controller: 'PortailCtrl2' } );
 
-		   $urlRouterProvider.otherwise( '/view1' );
+		   $urlRouterProvider.otherwise( '/mire' );
 	       } ] )
     .run( [ '$rootScope', '$location',
 	    function( $rootScope, $location ) {
@@ -25,17 +25,17 @@ angular.module( 'portailApp',
 		window.scope = $rootScope;
 	    } ] );
 
-$(document).ready( function() {
-    var client = new Faye.Client( '/portail/faye', {
-	timeout: 120
-    });
+// $(document).ready( function() {
+//     var client = new Faye.Client( '/portail/faye', {
+//	timeout: 120
+//     });
 
-    var subscription = client.subscribe('/foo', function(msg) {
-	console.log("message received on /foo : " + msg) ;
-	$.growl.notice({ duration: 6400, size: "large", title: "Hey there", message: "New message" });
-	$.growl({ message: msg.text });
-	// handle message
-    });
+//     var subscription = client.subscribe('/foo', function(msg) {
+//	console.log("message received on /foo : " + msg) ;
+//	$.growl.notice({ duration: 6400, size: "large", title: "Hey there", message: "New message" });
+//	$.growl({ message: msg.text });
+//	// handle message
+//     });
 
-    client.publish('/foo', {text: 'Hi there'});
-});
+//     client.publish('/foo', {text: 'Hi there'});
+// });
