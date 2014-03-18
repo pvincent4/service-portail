9a83f0c6a4657cb107670d32cb39b360ad291c1a
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-18 09:43:07 +0100

    apps définies dans le scope

--- public/app/scripts/controllers/PortailCtrl.js
+++ public/app/scripts/controllers/PortailCtrl.js
@@ -4,5 +4,33 @@ angular.module( 'portailApp.controllers' )
     .controller( 'PortailCtrl',
 		 [ '$scope',
 		   function( $scope ) {
-
+		       // Returns a random number between min and max
+		       var randInt = function( min, max ) {
+			   return Math.floor( Math.random(  ) * ( max - min ) + min );
+		       };
+		       $scope.couleurs = [ 'red',
+					   'blue',
+					   'yellow' ];
+		       $scope.apps = [
+			   { icone: '📓',
+			     couleur: $scope.couleurs[ randInt( 0, $scope.couleurs.length ) ],
+			     nom: 'Cahier de textes',
+			     lien: '/portail/#/show-app',
+			     notifications: [ { dummy: 'value' },
+					      { dummy: 'value' },
+					      { dummy: 'value' },
+					      { dummy: 'value' }
+					    ] },
+			   { icone: '☕',
+			     couleur: $scope.couleurs[ randInt( 0, $scope.couleurs.length ) ],
+			     nom: 'Café',
+			     lien: '/café',
+			     notifications: [ { dummy: 'value' }
+					    ] },
+			   { icone: '🎵',
+			     couleur: $scope.couleurs[ randInt( 0, $scope.couleurs.length ) ],
+			     nom: 'Musique',
+			     lien: '/musique',
+			     notifications: [  ] }
+		       ];
 		 } ] );
