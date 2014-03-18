0388212fc24255ecb0dbdf87dfabf4e66da9eb9e
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-18 17:04:21 +0100

    commencement de la prise en compte des infos utilisateur

--- public/app/scripts/controllers/PortailCtrl.js
+++ public/app/scripts/controllers/PortailCtrl.js
@@ -2,8 +2,8 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'PortailCtrl',
-		 [ '$scope',
-		   function( $scope ) {
+		 [ '$scope', 'currentUser',
+		   function( $scope, currentUser ) {
 		       // {{{ damier
 		       var nb_cases = 20;
 
@@ -17,43 +17,53 @@ angular.module( 'portailApp.controllers' )
 		       $scope.randCouleur = function(  ) {
 			   return $scope.couleurs[ _.random( 0, $scope.couleurs.length - 1 ) ];
 		       };
-		       $scope.apps = [
-			   { icone: '📓',
-			     couleur: $scope.randCouleur(),
-			     nom: 'Cahier de textes',
-			     lien: '/portail/#/show-app',
-			     notifications: [ { dummy: 'value' },
-					      { dummy: 'value' },
-					      { dummy: 'value' },
-					      { dummy: 'value' }
-					    ] },
-			   { icone: '☕',
-			     couleur: $scope.randCouleur(),
-			     nom: 'Café',
-			     lien: '/café',
-			     notifications: [ { dummy: 'value' }
-					    ] },
-			   { icone: '🎵',
-			     couleur: $scope.randCouleur(),
-			     nom: 'Musique',
-			     lien: '/musique',
-			     notifications: [  ] }
-		       ];
 
-		       // ajout cases vides
-		       _(nb_cases - $scope.apps.length).times( function() {
-			   $scope.apps.push( { icone: '',
-					       couleur: $scope.randCouleur(),
-					       nom: '',
-					       lien: '',
-					       notifications: [  ] } );
+		       currentUser.get().then( function( response ) {
+			   $scope.currentUser = response.data;
+
+			   $scope.apps = [];
+
+			   if ( $scope.currentUser.is_logged ) {
+			       // FIXME: utiliser de vrais données
+			       $scope.apps = [
+				   { icone: '📓',
+				     couleur: $scope.randCouleur(),
+				     nom: 'Cahier de textes',
+				     lien: '/portail/#/show-app',
+				     notifications: [ { dummy: 'value' },
+						      { dummy: 'value' },
+						      { dummy: 'value' },
+						      { dummy: 'value' }
+						    ] },
+				   { icone: '☕',
+				     couleur: $scope.randCouleur(),
+				     nom: 'Café',
+				     lien: '/café',
+				     notifications: [ { dummy: 'value' }
+						    ] },
+				   { icone: '🎵',
+				     couleur: $scope.randCouleur(),
+				     nom: 'Musique',
+				     lien: '/musique',
+				     notifications: [  ] }
+			       ];
+			   }
+
+			   // ajout cases vides
+			   _(nb_cases - $scope.apps.length).times( function() {
+			       $scope.apps.push( { icone: '',
+						   couleur: $scope.randCouleur(),
+						   nom: '',
+						   lien: '',
+						   notifications: [  ] } );
+			   });
+
+			   // randomize
+			   $scope.apps = _($scope.apps).shuffle();
+			   // }}}
+
+			   // {{{ aside
+			   // }}}
 		       });
 
-		       // randomize
-		       $scope.apps = _($scope.apps).shuffle();
-		       // }}}
-
-		       // {{{ aside
-
-		       // }}}
-		 } ] );
+		   } ] );
