3f3d555bcbe2869f4629c717e311f8d3ee5c2650
Gwenhael Le Moine <glemoine@erasme.org>
2014-03-25 12:03:24 +0100

    form profil

--- public/app/scripts/controllers/PortailUserCtrl.js
+++ public/app/scripts/controllers/PortailUserCtrl.js
@@ -4,6 +4,10 @@ angular.module( 'portailApp.controllers' )
     .controller( 'PortailUserCtrl',
 		 [ '$scope', 'currentUser',
 		   function( $scope, currentUser ) {
+		       $scope.groups = [ { ouvert: true },
+					 { ouvert: false },
+					 { ouvert: false },
+					 { ouvert: false } ];
 
 		       currentUser.get().then( function( response ) {
 			   $scope.current_user = response.data;
