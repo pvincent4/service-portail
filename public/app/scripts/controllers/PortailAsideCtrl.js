7c069a2972fbd2add3dd7d7b5c5476ea58da484f
Gwenhael Le Moine <glemoine@erasme.org>
2014-03-25 16:16:31 +0100

    gestion avatar dans le contrôleur

--- public/app/scripts/controllers/PortailAsideCtrl.js
+++ public/app/scripts/controllers/PortailAsideCtrl.js
@@ -6,6 +6,19 @@ angular.module( 'portailApp.controllers' )
 		   function( $scope, currentUser, news, APPLICATION_PREFIX ) {
 		       currentUser.get().then( function( response ) {
 			   $scope.current_user = response.data;
+			   $scope.avatar = '';
+
+			   if ( $scope.current_user.is_logged ) {
+			       // $scope.current_user['avatar'] = null //FIXME: debug
+			       // if ( $scope.current_user['avatar'] !== null ) {
+			       //     ;
+			       // } else
+			       if ( $scope.current_user['extra']['sexe'] == 'F' ) {
+				   $scope.avatar = '/app/bower_components/charte-graphique-laclasse-com/images/avatar_feminin.svg';
+			       } else {
+				   $scope.avatar = '/app/bower_components/charte-graphique-laclasse-com/images/avatar_masculin.svg';
+			       }
+			   }
 
 			   news.get().then( function( response ) {
 			       $scope.newsfeed = response.data;
