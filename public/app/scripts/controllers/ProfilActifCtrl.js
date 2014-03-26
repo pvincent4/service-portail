5c0358e5c874b07cae6ccf30aef4952f14a62776
Gwenhael Le Moine <glemoine@erasme.org>
2014-03-26 16:36:14 +0100

    répercute le changement de profil actif vers le serveur

--- public/app/scripts/controllers/ProfilActifCtrl.js
+++ public/app/scripts/controllers/ProfilActifCtrl.js
@@ -5,7 +5,7 @@ angular.module( 'portailApp.controllers' )
 		 [ '$scope', 'currentUser',
 		   function( $scope, currentUser ) {
 		       $scope.reload = function() {
-			   console.debug($scope.current_user['profil_actif'])
+			   $scope.current_user.$change_profil_actif({ index: $scope.current_user['profil_actif'] });
 		       };
 
 		       currentUser.get().then( function( response ) {
--- public/app/scripts/services/authentication.js
+++ public/app/scripts/services/authentication.js
@@ -5,7 +5,11 @@ angular.module( 'portailApp.services.authentication', [  ] );
 angular.module('portailApp.services.authentication')
     .factory('User', [ '$resource', 'APPLICATION_PREFIX',
 		       function( $resource, APPLICATION_PREFIX ) {
-			   return $resource( APPLICATION_PREFIX + '/api/user' );
+			   return $resource( APPLICATION_PREFIX + '/api/user',
+					     {},
+					     { change_profil_actif: { method: 'PUT',
+								      url: APPLICATION_PREFIX + '/api/user/profil_actif/:index',
+								      params: { index: '@index' } } } );
 		       } ] );
 
 angular.module( 'portailApp.services.authentication' )
