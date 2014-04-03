c0bc1ad4f642fa88fe648ac9af77144c9880578b
Gwenhael Le Moine <glemoine@erasme.org>
2014-04-03 16:45:36 +0200

    style

--- public/app/scripts/services/authentication.js
+++ public/app/scripts/services/authentication.js
@@ -3,19 +3,20 @@
 angular.module( 'portailApp.services.authentication', [  ] );
 
 angular.module('portailApp.services.authentication')
-    .factory('User', [ '$resource', 'APPLICATION_PREFIX',
-		       function( $resource, APPLICATION_PREFIX ) {
-			   return $resource( APPLICATION_PREFIX + '/api/user',
-					     {},
-					     { change_profil_actif: { method: 'PUT',
-								      url: APPLICATION_PREFIX + '/api/user/profil_actif/:index',
-								      params: { index: '@index' } } } );
-		       } ] );
+    .factory('User',
+	     [ '$resource', 'APPLICATION_PREFIX',
+	       function( $resource, APPLICATION_PREFIX ) {
+		   return $resource( APPLICATION_PREFIX + '/api/user',
+				     {},
+				     { change_profil_actif: { method: 'PUT',
+							      url: APPLICATION_PREFIX + '/api/user/profil_actif/:index',
+							      params: { index: '@index' } } } );
+	       } ] );
 
 angular.module( 'portailApp.services.authentication' )
     .service('currentUser',
-	     [ '$http', 'User',
-	       function( $http, User ) {
+	     [ 'User',
+	       function( User ) {
 		   this.get = _.memoize( function() {
 		       return User.get().$promise;
 		   } );
