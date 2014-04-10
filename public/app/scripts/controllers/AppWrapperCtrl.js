57f74b45c5f645f3bef28a2267f29d8310fec394
Pierre-Gilles Levallois <pgl@erasme.org>
2014-04-10 15:21:23 +0200

    ajout des librairies Faye et Growl

--- public/app/scripts/controllers/AppWrapperCtrl.js
+++ public/app/scripts/controllers/AppWrapperCtrl.js
@@ -1,26 +1,26 @@
 'use strict';
 
-angular.module( 'portailApp.controllers' )
-    .controller( 'AppWrapperCtrl',
-		 [ '$scope', '$http', '$stateParams', '$sce', 'currentUser', 'APPLICATION_PREFIX',
-		   function( $scope, $http, $stateParams, $sce, currentUser, APPLICATION_PREFIX ) {
-		       $scope.menu = [ { icone: 'logolaclasse.svg',
-					 texte: 'retour au portail',
-					 lien: APPLICATION_PREFIX + '/' },
-				       { icone: '12_aide.svg',
-					 texte: 'aide',
-					 lien: APPLICATION_PREFIX + '/' },
-				       { icone: '12_aide.svg',
-					 texte: 'se déconnecter',
-					 lien: APPLICATION_PREFIX + '/logout' } ];
+angular.module('portailApp.controllers')
+        .controller('AppWrapperCtrl',
+                ['$scope', '$http', '$stateParams', '$sce', 'currentUser', 'APPLICATION_PREFIX',
+                    function($scope, $http, $stateParams, $sce, currentUser, APPLICATION_PREFIX) {
+                        $scope.menu = [{icone: 'logolaclasse.svg',
+                                texte: 'retour au portail',
+                                lien: APPLICATION_PREFIX + '/'},
+                            {icone: '12_aide.svg',
+                                texte: 'aide',
+                                lien: APPLICATION_PREFIX + '/'},
+                            {icone: '12_aide.svg',
+                                texte: 'se déconnecter',
+                                lien: APPLICATION_PREFIX + '/logout'}];
 
-		       currentUser.get().then( function( response ) {
-			   $scope.current_user = response;
+                        currentUser.get().then(function(response) {
+                            $scope.current_user = response;
 
-			   currentUser.apps().then( function( response ) {
-			       var app = _(response).findWhere({id: $stateParams.app});
-			       $scope.app = { nom: app.nom,
-					      url: $sce.trustAsResourceUrl( app.url ) };
-			   } );
-		       } );
-		   } ] );
+                            currentUser.apps().then(function(response) {
+                                var app = _(response).findWhere({id: $stateParams.app});
+                                $scope.app = {nom: app.nom,
+                                    url: $sce.trustAsResourceUrl(app.url)};
+                            });
+                        });
+                    }]);
