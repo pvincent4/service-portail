ceb4181bebbb7f736fcd100a3c890df15aa3076a
Pierre-Gilles Levallois <pgl@erasme.org>
2014-04-17 18:14:24 +0200

    Prise en compte de application_prefix dans les chemins des images

--- public/app/scripts/controllers/Portail/AppsDamierCtrl.js
+++ public/app/scripts/controllers/Portail/AppsDamierCtrl.js
@@ -4,6 +4,7 @@ angular.module( 'portailApp.controllers' )
     .controller( 'PortailAppsDamierCtrl',
 		 [ '$scope', 'currentUser', 'news', 'APPLICATION_PREFIX',
 		   function( $scope, currentUser, news, APPLICATION_PREFIX ) {
+                       $scope.prefix = APPLICATION_PREFIX;
 		       currentUser.get().then( function( response ) {
 			   $scope.current_user = response;
 			   if ( $scope.current_user.is_logged ) {
