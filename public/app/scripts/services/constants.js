cb0d3aacc32b4fb5a7e338a98e625188d3cdc71d
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-27 17:16:19 +0200

    ignore constants.js because constants.js.sample

--- public/app/scripts/services/constants.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-/* constant for static pages routing */
-angular.module( 'portailApp.services.constants', [] );
-angular.module( 'portailApp.services.constants' ).constant( 'APP_PATH', '/v3' );
