2d663bf92296cd58c316e5ac249ea3ea0a34d7bd
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-28 12:30:06 +0200

    profil_actif est un tableau

--- public/app/scripts/controllers/ProfilActifCtrl.js
+++ public/app/scripts/controllers/ProfilActifCtrl.js
@@ -5,8 +5,8 @@ angular.module( 'portailApp.controllers' )
 		 [ '$scope', 'currentUser',
 		   function( $scope, currentUser ) {
 		       $scope.reload = function() {
-			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profils[ $scope.current_user.profil_actif ].profil_id,
-								       uai: $scope.current_user.profils[ $scope.current_user.profil_actif ].uai } );
+			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profil_actif[0].profil_id,
+								       uai: $scope.current_user.profil_actif[0].uai } );
 		       };
 
 		       currentUser.get().then( function( response ) {
