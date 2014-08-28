5fe285174b64f4b309f5a4c987f828e362744eb7
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-28 10:27:00 +0200

    set prefix hors de get_user

--- public/app/scripts/controllers/Portail/AsideCtrl.js
+++ public/app/scripts/controllers/Portail/AsideCtrl.js
@@ -4,15 +4,16 @@ angular.module( 'portailApp.controllers' )
     .controller( 'PortailAsideCtrl',
 		 [ '$scope', '$sce', 'currentUser', 'news', 'APP_PATH',
 		   function( $scope, $sce, currentUser, news, APP_PATH ) {
+		       $scope.prefix = APP_PATH;
+		       $scope.avatar = '';
+
 		       currentUser.get().then( function( response ) {
-                           $scope.prefix = APP_PATH;
 			   $scope.current_user = response;
-			   $scope.avatar = '';
 
 			   if ( $scope.current_user.is_logged ) {
-			        if ( $scope.current_user['avatar'] !== null ) {
-			            $scope.avatar = $scope.current_user['avatar'];
-			        } else
+				if ( $scope.current_user['avatar'] !== null ) {
+				    $scope.avatar = $scope.current_user['avatar'];
+				} else
 			       if ( $scope.current_user['sexe'] == 'F' ) {
 				   $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_feminin.svg';
 			       } else {
