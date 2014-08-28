0195b3bd27fe80749b6e145e2fdb050bf08c080c
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-28 15:07:05 +0200

    indentation

--- public/app/scripts/controllers/Portail/AppsDamierCtrl.js
+++ public/app/scripts/controllers/Portail/AppsDamierCtrl.js
@@ -4,7 +4,8 @@ angular.module( 'portailApp.controllers' )
     .controller( 'PortailAppsDamierCtrl',
 		 [ '$scope', 'currentUser', 'news', 'APP_PATH',
 		   function( $scope, currentUser, news, APP_PATH ) {
-                       $scope.prefix = APP_PATH;
+		       $scope.prefix = APP_PATH;
+
 		       currentUser.get().then( function( response ) {
 			   $scope.current_user = response;
 			   if ( $scope.current_user.is_logged ) {
