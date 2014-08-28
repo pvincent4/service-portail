b565520c6fc06a50fbc4e2bc659d3ec410b1d648
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-28 13:08:58 +0200

    reload après le changement de profil

--- public/app/scripts/controllers/ProfilActifCtrl.js
+++ public/app/scripts/controllers/ProfilActifCtrl.js
@@ -2,11 +2,14 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'ProfilActifCtrl',
-		 [ '$scope', 'currentUser',
-		   function( $scope, currentUser ) {
+		 [ '$scope', '$state', '$stateParams', 'currentUser',
+		   function( $scope, $state, $stateParams, currentUser ) {
 		       $scope.reload = function() {
 			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profil_actif[0].profil_id,
-								       uai: $scope.current_user.profil_actif[0].uai } );
+								       uai: $scope.current_user.profil_actif[0].uai } )
+			       .then( function() {
+				   $state.reload();
+			       });
 		       };
 
 		       currentUser.get().then( function( response ) {
