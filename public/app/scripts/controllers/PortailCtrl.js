9d14721cde8e33208234046fcc7e472589be043d
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-18 10:15:22 +0200

    intégration de la partie aside dans le portail principal (contrôleurs et vues)

--- public/app/scripts/controllers/PortailCtrl.js
+++ public/app/scripts/controllers/PortailCtrl.js
@@ -5,13 +5,31 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'PortailCtrl',
-		 [ '$scope', 'currentUser', 'APP_PATH',
-		   function( $scope, currentUser, APP_PATH ) {
+		 [ '$scope', '$sce', 'currentUser', 'APP_PATH', 'news',
+		   function( $scope, $sce, currentUser, APP_PATH, news ) {
 		       $scope.prefix = APP_PATH;
 		       // TODO : faire une factory et un service pour les annonces.
 		       // L'idée est d'aller lire le flux twitter @laclasse avec le hash #sys
 		       $scope.annonce = ""; //"En moment sur Laclasse.com : La version 3 sort des cartons !";
 		       currentUser.get().then( function( response ) {
 			   $scope.current_user = response;
+
+			   if ( $scope.current_user.is_logged ) {
+			       if ( $scope.current_user['avatar'] !== null ) {
+				   $scope.avatar = $scope.current_user['avatar'];
+			       } else {
+				   if ( $scope.current_user['sexe'] == 'F' ) {
+				       $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_feminin.svg';
+				   } else {
+				       $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_masculin.svg';
+				   }
+			       }
+			       news.get().then( function( response ) {
+				   $scope.newsfeed = _(response.data).map( function( item ) {
+				       item.trusted_description = $sce.trustAsHtml( item.description );
+				       return item;
+				   });
+			       });
+			   }
 		       });
 		   } ] );
