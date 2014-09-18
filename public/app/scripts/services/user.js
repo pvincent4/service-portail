30bf3d1268cf6de67e8d87b22ff1f9588e17b74c
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-18 10:15:40 +0200

    memoize l'appel à l'API user

--- public/app/scripts/services/user.js
+++ public/app/scripts/services/user.js
@@ -26,6 +26,6 @@ angular.module( 'portailApp.services.user' )
     .service('currentUser',
 	     [ 'User', 'UserApps',
 	       function( User, UserApps ) {
-		   this.get = function() { return User.get().$promise; };
+		   this.get = _.memoize ( function() { return User.get().$promise; } );
 		   this.apps = function() { return UserApps.query().$promise; };
 	       } ] );
