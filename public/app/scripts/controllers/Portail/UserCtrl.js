789d04ee1a25f5b4884a45a21811cb55213d159f
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-06 12:26:39 +0200

    chargement des données dans des resolve

--- public/app/scripts/controllers/Portail/UserCtrl.js
+++ public/app/scripts/controllers/Portail/UserCtrl.js
@@ -2,8 +2,8 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'PortailUserCtrl',
-		 [ '$scope', '$state', 'currentUser', 'APP_PATH',
-		   function( $scope, $state, currentUser, APP_PATH ) {
+		 [ '$scope', '$state', 'current_user', 'currentUser', 'APP_PATH',
+		   function( $scope, $state, current_user, currentUser, APP_PATH ) {
 		       $scope.prefix = APP_PATH;
 		       $scope.groups = [ { ouvert: true,
 					   enabled: true },
@@ -14,24 +14,22 @@ angular.module( 'portailApp.controllers' )
 					 { ouvert: false,
 					   enabled: false } ];
 
-		       currentUser.get().then( function( response ) {
-			   $scope.current_user = response;
-			   $scope.current_user.editable = _($scope.current_user.id_jointure_aaf).isNull();
+		       $scope.current_user = current_user;
+		       $scope.current_user.editable = _($scope.current_user.id_jointure_aaf).isNull();
 
-			   $scope.new_avatar = function( flowFile ) {
-			       $scope.current_user.new_avatar = flowFile.file;
-			   };
+		       $scope.new_avatar = function( flowFile ) {
+			   $scope.current_user.new_avatar = flowFile.file;
+		       };
 
-			   $scope.fermer = function( sauvegarder ) {
-			       if ( sauvegarder ) {
-				   if ( !_($scope.current_user.new_avatar).isNull() ) {
-				       currentUser.avatar.upload( $scope.current_user.new_avatar );
-				   }
-
-				   $scope.current_user.$update();
+		       $scope.fermer = function( sauvegarder ) {
+			   if ( sauvegarder ) {
+			       if ( !_($scope.current_user.new_avatar).isNull() ) {
+				   currentUser.avatar.upload( $scope.current_user.new_avatar );
 			       }
 
-			       $state.go( 'portail.logged' );
-			   };
-		       });
+			       $scope.current_user.$update();
+			   }
+
+			   $state.go( 'portail.logged' );
+		       };
 		   } ] );
--- public/app/scripts/controllers/PortailCtrl.js
+++ public/app/scripts/controllers/PortailCtrl.js
@@ -5,31 +5,30 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'PortailCtrl',
-		 [ '$scope', '$sce', 'currentUser', 'APP_PATH', 'news',
-		   function( $scope, $sce, currentUser, APP_PATH, news ) {
+		 [ '$scope', '$sce', 'current_user', 'APP_PATH', 'news',
+		   function( $scope, $sce, current_user, APP_PATH, news ) {
 		       $scope.prefix = APP_PATH;
+		       $scope.current_user = current_user;
+
 		       // TODO : faire une factory et un service pour les annonces.
 		       // L'idée est d'aller lire le flux twitter @laclasse avec le hash #sys
 		       $scope.annonce = ""; //"En moment sur Laclasse.com : La version 3 sort des cartons !";
-		       currentUser.get().then( function( response ) {
-			   $scope.current_user = response;
 
-			   if ( $scope.current_user.is_logged ) {
-			       if ( $scope.current_user['avatar'] !== null ) {
-				   $scope.avatar = $scope.current_user['avatar'];
+		       if ( $scope.current_user.is_logged ) {
+			   if ( $scope.current_user['avatar'] !== null ) {
+			       $scope.avatar = $scope.current_user['avatar'];
+			   } else {
+			       if ( $scope.current_user['sexe'] == 'F' ) {
+				   $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_feminin.svg';
 			       } else {
-				   if ( $scope.current_user['sexe'] == 'F' ) {
-				       $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_feminin.svg';
-				   } else {
-				       $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_masculin.svg';
-				   }
+				   $scope.avatar = APP_PATH + '/bower_components/charte-graphique-laclasse-com/images/avatar_masculin.svg';
 			       }
-			       news.get().then( function( response ) {
-				   $scope.newsfeed = _(response.data).map( function( item ) {
-				       item.trusted_description = $sce.trustAsHtml( item.description );
-				       return item;
-				   });
-			       });
 			   }
-		       });
+			   news.get().then( function( response ) {
+			       $scope.newsfeed = _(response.data).map( function( item ) {
+				   item.trusted_description = $sce.trustAsHtml( item.description );
+				   return item;
+			       });
+			   });
+		       }
 		   } ] );
