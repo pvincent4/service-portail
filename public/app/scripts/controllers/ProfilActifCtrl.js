4ffe20024dfa21eabbd1d80d1164c3a804cc6592
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-06 12:05:50 +0200

    vrai reload qui marche même au premier changement

--- public/app/scripts/controllers/ProfilActifCtrl.js
+++ public/app/scripts/controllers/ProfilActifCtrl.js
@@ -2,13 +2,13 @@
 
 angular.module( 'portailApp.controllers' )
     .controller( 'ProfilActifCtrl',
-		 [ '$scope', '$state', 'currentUser',
-		   function( $scope, $state, currentUser ) {
+		 [ '$scope', '$state', '$stateParams', 'currentUser',
+		   function( $scope, $state, $stateParams, currentUser ) {
 		       $scope.reload = function() {
 			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profil_actif[0].profil_id,
 								       uai: $scope.current_user.profil_actif[0].uai } )
 			       .then( function() {
-				   $state.reload();
+				   $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
 			       });
 		       };
 
