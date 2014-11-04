8ded66add594978086fff2d6b192579f8e0e8fe6
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-04 16:45:53 +0100

    premier jet de paramétrage par application dans le portail

--- public/app/js/controllers/Portail/AppsDamierCtrl.js
+++ public/app/js/controllers/Portail/AppsDamierCtrl.js
@@ -7,5 +7,11 @@ angular.module( 'portailApp' )
 		       $scope.prefix = APP_PATH;
 
 		       $scope.current_user = current_user;
-		       $scope.apps = current_apps;
+		       $scope.apps = current_apps.map( function( app ) {
+			   app.configure = false;
+			   app.toggle_configure = function() { app.configure = !app.configure; };
+			   app.deactivate = function() { console.log( app.nom + ' désactivée.' ); };
+
+			   return app;
+		       } );
 		   } ] );
