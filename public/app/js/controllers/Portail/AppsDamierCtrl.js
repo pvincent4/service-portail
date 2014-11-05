d61ed3e5f1ff9da82554ceec91efa9d20dc4d0bf
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-05 15:33:16 +0100

    définition des couleurs et ordre par empilement

--- public/app/js/controllers/Portail/AppsDamierCtrl.js
+++ public/app/js/controllers/Portail/AppsDamierCtrl.js
@@ -2,16 +2,20 @@
 
 angular.module( 'portailApp' )
     .controller( 'PortailAppsDamierCtrl',
-		 [ '$scope', 'current_user', 'current_apps', 'APP_PATH',
-		   function( $scope, current_user, current_apps, APP_PATH ) {
+		 [ '$scope', 'current_user', 'current_apps', 'APP_PATH', 'CASES',
+		   function( $scope, current_user, current_apps, APP_PATH, CASES ) {
 		       $scope.prefix = APP_PATH;
 
 		       $scope.current_user = current_user;
-		       $scope.apps = current_apps.map( function( app ) {
-			   app.configure = false;
-			   app.toggle_configure = function() { app.configure = !app.configure; };
-			   app.deactivate = function() { console.log( app.nom + ' désactivée.' ); };
 
-			   return app;
-		       } );
+		       $scope.cases = CASES;
+
+		       _.chain(current_apps)
+			   .select( function( app ) { return app.active; } )
+			   .each( function( app, i ) {
+			       $scope.cases[ i ].app = app;
+			       $scope.cases[ i ].app.configure = false;
+			       $scope.cases[ i ].app.toggle_configure = function() { app.configure = !app.configure; };
+			       $scope.cases[ i ].app.deactivate = function() { console.log( app.nom + ' désactivée.' ); };
+			   } );
 		   } ] );
