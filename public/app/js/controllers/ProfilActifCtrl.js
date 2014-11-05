9c968dd73b7128c86e686308c62db990158a91da
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-05 14:53:20 +0100

    refactoring, trop de choses pour m'en rappeler

--- public/app/js/controllers/ProfilActifCtrl.js
+++ public/app/js/controllers/ProfilActifCtrl.js
@@ -5,8 +5,8 @@ angular.module( 'portailApp' )
 		 [ '$scope', '$state', '$stateParams', 'currentUser',
 		   function( $scope, $state, $stateParams, currentUser ) {
 		       $scope.reload = function() {
-			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profil_actif[0].profil_id,
-								       uai: $scope.current_user.profil_actif[0].uai } )
+			   $scope.current_user.$change_profil_actif( { profil_id: $scope.current_user.profil_actif.profil_id,
+								       uai: $scope.current_user.profil_actif.uai } )
 			       .then( function() {
 				   $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
 			       });
