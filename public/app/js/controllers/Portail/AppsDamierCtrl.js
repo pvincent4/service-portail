56a717bc656ba604c2352a91465263f2c8e99faf
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-10 16:13:48 +0100

    cablage modification des apps Angular->Ruby

--- public/app/js/controllers/Portail/AppsDamierCtrl.js
+++ public/app/js/controllers/Portail/AppsDamierCtrl.js
@@ -14,26 +14,36 @@ angular.module( 'portailApp' )
 			   containment: '.damier',
 			   handle: '.handle',
 			   'ui-floating': true,
-			   update: function(e, ui) {
-			       console.log( 'Update' );
-			   },
-			   stop: function(e, ui) {
-			       console.log( 'Stop' );
+			   stop: function( e, ui ) {
+			       console.log( $scope.cases );
+			       _($scope.cases).each( function( c, i ) {
+				   if ( _(c).has( 'app' ) ) {
+				       c.app.index = i;
+				       c.app.$update();
+				   }
+			       } );
 			   }
 		       };
+
 		       $scope.toggle_modification = function() {
 			   $scope.modification = !$scope.modification;
 			   $scope.sortable_options.disabled = !$scope.sortable_options.disabled;
+			   if ( !$scope.modification ) {
+			       _($scope.cases).each( function( c ) {
+				   if ( _(c).has( 'app' ) ) {
+				       c.app.configure = false;
+				   }
+			       } );
+			   }
 		       };
 
-		       _.chain(current_apps)
-			   .select( function( app ) { return app.active; } )
-			   .each( function( app, i ) {
-			       $scope.cases[ i ].app = app;
-			       $scope.cases[ i ].app.configure = false;
-			       $scope.cases[ i ].app.tmp_active = $scope.cases[ i ].app.active;
+		   _.chain(current_apps)
+		   .select( function( app ) { return app.active; } )
+		   .each( function( app, i ) {
+		       $scope.cases[ i ].app = app;
+		       $scope.cases[ i ].app.configure = false;
+		       $scope.cases[ i ].app.tmp_active = $scope.cases[ i ].app.active;
 
-			       $scope.cases[ i ].app.toggle_configure = function() { app.configure = !app.configure; };
-			       $scope.cases[ i ].app.deactivate = function() { console.log( app.nom + ' désactivée.' ); };
-			   } );
-		   } ] );
+		       $scope.cases[ i ].app.toggle_configure = function() { app.configure = !app.configure; };
+		   } );
+		 } ] );
