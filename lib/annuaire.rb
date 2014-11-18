b1391165ec88e8b9d1eadcc3f2b0636b723cb5be
Pierre-Gilles Levallois <pgl@erasme.org>
2014-11-18 11:09:18 +0100

    ménage et commentaires

--- lib/annuaire.rb
+++ lib/annuaire.rb
@@ -8,194 +8,34 @@ require 'openssl'
 require_relative '../config/options'
 
 # Module d'interfaçage avec l'annuaire
+# Ce module s'appuye sur la gem de signature et de communicationavec l'annuaire.
+#
 module AnnuaireSpe
   module_function
-
-#  @coordination = nil
-#  @liaison = nil
-#  @search = false
-
-  # Petit setter pour les tests unitaires
-#  def set_search(arg)
-#    @search = arg
-#  end
-
-#  # Fonction de vérification du mode d'api paramétrée dans la conf et init des paramètres
-#  def init
-#    @coordination = '?'
-#    @liaison = '/'
-#    if ANNUAIRE[:api_mode] == 'v2'
-#      @coordination = '&'
-#      @liaison = ''
-#    end
-#  end
-
-  # Construire la chaine de paramètres à encoder
-#  def build_canonical_string(args)
-#    init
-#    s = Hash[ args.sort ].map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
-#
-#    "#{@coordination}#{s};"
-#  end
-
-  # Construction de la signature
-#  def build_signature(canonical_string, ts)
-#    digest = OpenSSL::Digest.new( 'sha1' )
-#    digested_message = Base64.encode64( OpenSSL::HMAC.digest( digest, ANNUAIRE[:api_key], canonical_string ) )
-#    { app_id: ANNUAIRE[:app_id],
-#      timestamp: ts,
-#      signature: digested_message }.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( ';' ).chomp
-#  end
-
-  # Compatibilité api V2, rectification du service
-  # qui ne doit pas être au format REST, mais au format URL
-#  def compat_service(srv)
-#    if ANNUAIRE[:api_mode] == 'v2'
-#      if @search
-#        srv.sub! 'users', 'SearchUsers&uid='
-#        srv.sub! 'matieres/libelle', 'Matieres&lib='
-#      else
-#        srv.sub! 'users', 'Users&uid='
-#        srv.sub! 'etablissements', 'Etablissements&uai='
-#        srv.sub! 'matieres', 'Matieres&code='
-#        srv.sub! 'regroupements', 'Regroupements&grp_id='
-#        # En dernier pour traiter les cas des etab/[uai]/Regroupements et users/[uid]/Regroupements
-#        # Les services Users et Etablissements, donnent l'info des regroupements en mode expand.
-#        srv.sub! '/Regroupements&grp_id=', '&expand=true' if ( !srv.match('Users&uid=').nil? || !srv.match('Etablissements&uai=').nil? )
-#      end
-#      srv.sub! '/', '' # Supprimer les '/' devant les data : &grp_id=/19 devient &grp_id=19
-#    end
-#    srv
-#  end
-
-#  def sign( uri, service, args )
-#    init
-#    timestamp = Time.now.getutc.strftime('%FT%T')
-#    canonical_string = ANNUAIRE[:api_mode] == 'v3' ? "#{uri}#{@liaison}#{service}" : ''
-#    canonical_string += build_canonical_string( args )
-#    canonical_string += "#{timestamp};#{ANNUAIRE[:app_id]}" if ANNUAIRE[:api_mode] == 'v3'
-#
-#    signature = build_signature( canonical_string, timestamp )
-#
-#    # Compatibilité avec les api laclasse v2 (pl/sql): pas de mode REST, en fait.
-#    service = compat_service( service )
-#    # Fin patch compat.
-#
-#    query = args.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
-#
-#    puts "#{uri}#{@liaison}#{service}#{@coordination}#{query};#{signature}"
-#    "#{uri}#{@liaison}#{service}#{@coordination}#{query};#{signature}"
-#  end
-
-#  def send_request( service, param, expand, error_msg )
-#    RestClient.get( sign( ANNUAIRE[:url], "#{service}/#{CGI.escape( param )}", { expand: expand } ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )
-#      else
-#        STDERR.puts "#{error_msg} : #{CGI.escape( param )}"
-#      end
-#    end
-#  end
-
-#  def search_matiere( label )
-#    label = URI.escape( label )
-#    @search = true
-#    RestClient.get( sign( ANNUAIRE[:url], "matieres/libelle/#{label}", {} ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )
-#      else
-#        STDERR.puts "Matière inconnue : #{label}"
-#        STDERR.puts 'URL fautive: ' + sign( ANNUAIRE[:url], "/matieres/libelle/#{label}", {} )
-#        return { 'id' => nil }
-#      end
-#    end
-#  end
-
-#  def search_regroupement( code_uai, nom )
-#    code_uai = URI.escape( code_uai )
-#    nom = URI.escape( nom )
-#    @search = true
-#    RestClient.get( sign( ANNUAIRE[:url], 'regroupement', { etablissement: code_uai, nom: nom, expand: "false" } ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Regroupement inconnu : #{nom}"
-#        return { 'id' => nil }
-#      end
-#    end
-#  end
-
-#  def search_utilisateur( code_uai, nom, prenom )
-#    code_uai = URI.escape( code_uai )
-#    nom = URI.escape( nom )
-#    prenom = URI.escape( prenom )
-#    @search = true
-#    RestClient.get( sign( ANNUAIRE[:url], 'users', { nom: nom, prenom: prenom, etablissement: code_uai } ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Utilisateur inconnu : #{prenom} #{nom}"
-#        return { 'id_ent' => nil }
-#      end
-#    end
-#  end
-
-#  # API d'interfaçage avec l'annuaire à destination du client
-#  def get_matiere( id )
-#    @search = false
-#    send_request 'matieres', CGI.escape( id ), 'false', 'Matière inconnue'
-#  end
-
+  
   # Service classes et groupes d'élèves
   def get_regroupement( id )
-#    @search = false
-#    send_request 'regroupements', CGI.escape( id ), 'false', 'Regroupement inconnu'
     response = Annuaire.send_request_signed(ANNUAIRE[:url], ANNUAIRE[:service_annuaire_regroupement] + id, {"expand" => "true"}) 
   end
 
   # Service Utilisateur : init de la session et de son environnement
   def get_user( uid )
-#    @search = false
-#    send_request 'users', CGI.escape( uid ), 'true', 'User inconnu'
     response = Annuaire.send_request_signed(ANNUAIRE[:url], ANNUAIRE[:service_annuaire_user] + uid, {"expand" => "true"}) 
   end
 
   # Service Utilisateur : récupération des ressources numériques de l'utilisateur
   def get_user_resources( uid )
-#   @search = false
-#    uid = URI.escape( uid )
-#    RestClient.get( sign( ANNUAIRE[:url], "users/#{uid}/ressources", {} ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )
-#      else
-#        STDERR.puts "erreur getting user's resources : #{CGI.escape( uid )}"
-#      end
-#    end
     response = Annuaire.send_request_signed(ANNUAIRE[:url], "app/users/#{uid}/ressources", {"expand" => "true"}) 
   end
 
+  # Liste des regroupements de l'utilisateur connecté
   def get_user_regroupements( uid )
-#    @search = false
-#    uid = URI.escape( uid )
-#    RestClient.get( sign( ANNUAIRE[:url], "users/#{uid}/regroupements", {} ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )
-#      else
-#        STDERR.puts "erreur getting user's regroupements : #{CGI.escape( uid )}"
-#      end
-#    end
     response = Annuaire.send_request_signed(ANNUAIRE[:url], "app/users/#{uid}/regroupements", {"expand" => "true"}) 
   end
 
+  # Modification des données de l'utilisateur connecté
   def put_user( uid, params )
     uid = URI.escape( uid )
-
     params.each do |key, _value|
       if params[ key ].is_a? String
         params[ key ] = URI.escape( params[ key ] )
@@ -205,86 +45,36 @@ module AnnuaireSpe
         params[ key ] = URI.escape( params[ key ].to_s )
       end
     end
-    
     response = Annuaire.put_request_signed(ANNUAIRE[:url], "app/users/#{uid}", params ) 
-
-#    RestClient.put( sign( ANNUAIRE[:url], "users/#{uid}", params ), '' ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Error updating user #{uid}"
-#        return { 'id' => nil }
-#      end
-#    end
   end
 
+  # Modification avatar
   def put_user_avatar( uid, image )
     uid = URI.escape( uid )
-
     response = Annuaire.post_request_signed(ANNUAIRE[:url], "app/users/#{uid}/upload/avatar", {}, image: Base64.encode64( image ) ) 
-#    RestClient.post( sign( ANNUAIRE[:url], "users/#{uid}/upload/avatar", {} ),
-#                     image: Base64.encode64( image ) ) do
-#      |response, _request, _result|
-#      if response.code == 201
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Error uploading avatar for user #{uid}"
-#        return { 'id' => nil }
-#      end
-#    end
   end
 
+  # Suppression avatar
   def delete_user_avatar( uid )
     uid = URI.escape( uid )
-
     response = Annuaire.delete_request_signed(ANNUAIRE[:url], "app/users/#{uid}/avatar", {} ) 
-#    RestClient.delete( sign( ANNUAIRE[:url], "users/#{uid}/avatar", {} ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Error deleting avatar for user #{uid}"
-#        return { 'id' => nil }
-#      end
-#    end
   end
 
+  # Modification du profil actif de l'utilisateur connecté
   def put_user_profil_actif( uid, profil_id, code_uai )
     uid = URI.escape( uid )
     profil_id = URI.escape( profil_id )
     code_uai = URI.escape( code_uai )
-
    response = Annuaire.put_request_signed(ANNUAIRE[:url], "app/users/#{uid}/profil_actif", uai: code_uai, profil_id: profil_id ) 
-#   RestClient.put( sign( ANNUAIRE[:url], "users/#{uid}/profil_actif", uai: code_uai, profil_id: profil_id ), '' ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )[0]
-#      else
-#        STDERR.puts "Error setting profil_actif to #{profil_id} for user #{uid} and etablissement #{code_uai}"
-#        return { 'id' => nil }
-#      end
-#    end
   end
 
-  # Service etablissement
+  # Liste des personnels d'un etablissement
   def get_etablissement( uai )
-#    @search = false
-#    send_request 'etablissements', CGI.escape( uai ), 'true', 'Etablissement inconnu'
     response = Annuaire.send_request_signed(ANNUAIRE[:url], ANNUAIRE[:service_annuaire_personnel] + uai, {"expand" => "true"}) 
   end
 
+  # Liste des regroupements d'un établissement
   def get_etablissement_regroupements( uai )
-#    @search = false
-#    RestClient.get( sign( ANNUAIRE[:url], "etablissements/0690078K/users", {} ) ) do
-#      |response, _request, _result|
-#      if response.code == 200
-#        return JSON.parse( response )
-#      else
-#        STDERR.puts "erreur getting etablissement's regroupements : #{CGI.escape( uai )}"
-#      end
-#    end
     response = Annuaire.send_request_signed(ANNUAIRE[:url], ANNUAIRE[:service_annuaire_personnel] + uai + '/users', {"expand" => "true"}) 
-
   end
 end
