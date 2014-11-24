207e98789487ce77bd64c59b80fb76a24a20882a
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-24 15:16:23 +0100

    récupération des apps déplacé dans le contrôleur du damier

--- public/app/js/controllers/PortailAppsDamierCtrl.js
+++ public/app/js/controllers/PortailAppsDamierCtrl.js
@@ -2,8 +2,8 @@
 
 angular.module( 'portailApp' )
     .controller( 'PortailAppsDamierCtrl',
-		 [ '$scope', '$modal', '$log', 'current_user', 'current_apps', 'APP_PATH', 'CASES',
-		   function( $scope, $modal, $log, current_user, current_apps, APP_PATH, CASES ) {
+		 [ '$scope', '$modal', '$log', 'current_user', 'currentUser', 'APP_PATH', 'CASES',
+		   function( $scope, $modal, $log, current_user, currentUser, APP_PATH, CASES ) {
 		       $scope.prefix = APP_PATH;
 		       $scope.current_user = current_user;
 		       $scope.cases = CASES;
@@ -44,71 +44,75 @@ angular.module( 'portailApp' )
 			   }
 		       };
 
-		       $scope.add_tile = function() {
-			   $modal.open( {
-			       templateUrl: 'views/popup_ajout_app.html',
-			       controller: 'PopupAjoutAppCtrl',
-			       resolve: {
-				   apps: function () {
-				       return _(current_apps).select( function( app ) { return !app.active; } );
-				   }
-			       }
-			   } )
-			       .result.then( function( new_app ) {
-				   new_app = tool_app( new_app );
+		       currentUser.apps()
+			   .then( function( response ) {
+			       $scope.current_apps = response;
+			       $scope.add_tile = function() {
+				   $modal.open( {
+				       templateUrl: 'views/popup_ajout_app.html',
+				       controller: 'PopupAjoutAppCtrl',
+				       resolve: {
+					   apps: function () {
+					       return _($scope.current_apps).select( function( app ) { return !app.active; } );
+					   }
+				       }
+				   } )
+				       .result.then( function( new_app ) {
+					   new_app = tool_app( new_app );
 
-				   if ( new_app.creation ) {
-				       new_app.configure = true;
-				       _.chain($scope.cases)
-					   .select( function( c ) { return !_(c).has( 'app' ); } )
-					   .first()
-					   .value().app = new_app;
-				   }
+					   if ( new_app.creation ) {
+					       new_app.configure = true;
+					       _.chain($scope.cases)
+						   .select( function( c ) { return !_(c).has( 'app' ); } )
+						   .first()
+						   .value().app = new_app;
+					   }
 
-				   new_app.active = true;
-			       } );
-		       };
+					   new_app.active = true;
+				       } );
+			       };
 
-		       $scope.toggle_modification = function( save ) {
-			   $scope.modification = !$scope.modification;
-			   $scope.sortable_options.disabled = !$scope.sortable_options.disabled;
-			   if ( $scope.modification ) {
-			       _(current_apps).each( function( app ) {
-				   app.draft = angular.copy( app );
-			       } );
-			   } else {
-			       _($scope.cases).each( function( c ) {
-				   if ( _(c).has( 'app' ) ) {
-				       c.app.configure = false;
-				       if ( save && c.app.dirty ) {
-					   _.chain(c.app.draft)
-					       .keys()
-					       .each( function( key ) {
-						   c.app[ key ] = angular.copy( c.app.draft[ key ] );
-					       } );
-					   c.app.$update();
-					   c.app = tool_app( c.app );
+			       $scope.toggle_modification = function( save ) {
+				   $scope.modification = !$scope.modification;
+				   $scope.sortable_options.disabled = !$scope.sortable_options.disabled;
+				   if ( $scope.modification ) {
+				       _($scope.current_apps).each( function( app ) {
+					   app.draft = angular.copy( app );
+				       } );
+				   } else {
+				       _($scope.cases).each( function( c ) {
+					   if ( _(c).has( 'app' ) ) {
+					       c.app.configure = false;
+					       if ( save && c.app.dirty ) {
+						   _.chain(c.app.draft)
+						       .keys()
+						       .each( function( key ) {
+							   c.app[ key ] = angular.copy( c.app.draft[ key ] );
+						       } );
+						   c.app.$update();
+						   c.app = tool_app( c.app );
+					       }
+					   }
+				       } );
+
+				       if ( save && apps_indexes_changed ) {
+					   // mise à jour de l'annuaire avec les nouveaux index des apps suite au déplacement
+					   _($scope.cases).each( function( c, i ) {
+					       if ( _(c).has( 'app' ) ) {
+						   c.app.index = i;
+						   c.app.$update();
+					       }
+					   } );
 				       }
 				   }
-			       } );
+			       };
 
-			       if ( save && apps_indexes_changed ) {
-				   // mise à jour de l'annuaire avec les nouveaux index des apps suite au déplacement
-				   _($scope.cases).each( function( c, i ) {
-				       if ( _(c).has( 'app' ) ) {
-					   c.app.index = i;
-					   c.app.$update();
-				       }
-				   } );
-			       }
-			   }
-		       };
-
-		       current_apps.$promise.then( function() {
-			   _.chain(current_apps)
-			       .sortBy( function( app ) { return !app.active; } )
-			       .each( function( app, i ) {
-				   $scope.cases[ i ].app = tool_app( app );
+			       $scope.current_apps.$promise.then( function() {
+				   _.chain($scope.current_apps)
+				       .sortBy( function( app ) { return !app.active; } )
+				       .each( function( app, i ) {
+					   $scope.cases[ i ].app = tool_app( app );
+				       } );
 			       } );
-		       } );
+			   } );
 		   } ] );
