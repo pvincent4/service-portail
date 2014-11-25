2dd6859919d20046371dba9046c80b09edf96a10
Gwenhael Le Moine <glemoine@erasme.org>
2014-11-25 11:27:33 +0100

    gem annuaire mis à jour, dont acte

--- lib/annuaire.rb
+++ lib/annuaire.rb
@@ -15,22 +15,22 @@ module AnnuaireSpe
 
   # Service classes et groupes d'élèves
   def get_regroupement( id )
-    Annuaire.send_request_signed( ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_regroupement]}#{id}", 'expand' => 'true' )
+    Annuaire.send_request_signed( :service_annuaire_regroupement, "#{id}", 'expand' => 'true' )
   end
 
   # Service Utilisateur : init de la session et de son environnement
   def get_user( uid )
-    Annuaire.send_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}", 'expand' => 'true')
+    Annuaire.send_request_signed( :service_annuaire_user, "#{uid}", 'expand' => 'true' )
   end
 
   # Service Utilisateur : récupération des ressources numériques de l'utilisateur
   def get_user_resources( uid )
-    Annuaire.send_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}/ressources", 'expand' => 'true')
+    Annuaire.send_request_signed(:service_annuaire_user, "#{uid}/ressources", 'expand' => 'true')
   end
 
   # Liste des regroupements de l'utilisateur connecté
   def get_user_regroupements( uid )
-    Annuaire.send_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}/regroupements", 'expand' => 'true')
+    Annuaire.send_request_signed(:service_annuaire_user, "#{uid}/regroupements", 'expand' => 'true')
   end
 
   # Modification des données de l'utilisateur connecté
@@ -45,19 +45,19 @@ module AnnuaireSpe
         params[ key ] = URI.escape( params[ key ].to_s )
       end
     end
-    Annuaire.put_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}", params )
+    Annuaire.put_request_signed(:service_annuaire_user, "#{uid}", params )
   end
 
   # Modification avatar
   def put_user_avatar( uid, image )
     uid = URI.escape( uid )
-    Annuaire.post_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}/upload/avatar", {}, image: Base64.encode64( image ) )
+    Annuaire.post_request_signed( :service_annuaire_user, "#{uid}/upload/avatar", {}, image: image )
   end
 
   # Suppression avatar
   def delete_user_avatar( uid )
     uid = URI.escape( uid )
-    Annuaire.delete_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}/avatar", {} )
+    Annuaire.delete_request_signed(:service_annuaire_user, "#{uid}/avatar", {} )
   end
 
   # Modification du profil actif de l'utilisateur connecté
@@ -65,16 +65,16 @@ module AnnuaireSpe
     uid = URI.escape( uid )
     profil_id = URI.escape( profil_id )
     code_uai = URI.escape( code_uai )
-    Annuaire.put_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_user]}#{uid}/profil_actif", uai: code_uai, profil_id: profil_id )
+    Annuaire.put_request_signed(:service_annuaire_user, "#{uid}/profil_actif", uai: code_uai, profil_id: profil_id )
   end
 
   # Liste des personnels d'un etablissement
   def get_etablissement( uai )
-    Annuaire.send_request_signed(ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_personnel]}#{uai}", 'expand' => 'true')
+    Annuaire.send_request_signed(:service_annuaire_personnel, "#{uai}", 'expand' => 'true')
   end
 
   # Liste des regroupements d'un établissement
   def get_etablissement_regroupements( uai )
-    Annuaire.send_request_signed( ANNUAIRE[:url], "#{ANNUAIRE[:service_annuaire_personnel]}#{uai}/users", 'expand' => 'true' )
+    Annuaire.send_request_signed( :service_annuaire_personnel, "#{uai}/users", 'expand' => 'true' )
   end
 end
