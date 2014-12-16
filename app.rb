5bad1432b5c459c1e6c46590ebcccccf45ba0612
Gwenhael Le Moine <glemoine@erasme.org>
2014-12-16 10:43:19 +0100

    pas de layout au redirect

--- app.rb
+++ app.rb
@@ -60,7 +60,7 @@ class SinatraApp < Sinatra::Base
   get "#{APP_PATH}/redirect/" do
     param :url, String, required: true
 
-    erb :redirect, locals: { url: params[:url] }
+    erb :redirect, locals: { url: params[:url] }, layout: false
   end
 
   ##### API ####################################################################
