e05bcd6be1f529c9230efb3c6a92011fa67fc1a9
Gwenhael Le Moine <glemoine@erasme.org>
2015-01-14 16:02:25 +0100

    interface de gestion des flux RSS par établissement, fixes #50

--- public/app/js/controllers/PopupConfigNewsFluxesCtrl.js
+++ public/app/js/controllers/PopupConfigNewsFluxesCtrl.js
@@ -4,10 +4,27 @@ angular.module( 'portailApp' )
     .controller( 'PopupConfigNewsFluxesCtrl',
 		 [ '$scope', '$modalInstance', 'Flux',
 		   function( $scope, $modalInstance, Flux ) {
-		       $scope.current_flux = Flux.query();
+		       Flux.query().$promise.then( function( response ) {
+			   $scope.current_flux = _(response).map( function( flux ) {
+			       flux.dirty = false;
+
+			       return flux;
+			   } );
+		       } );
+
+		       $scope.minutes = _.range( 1, 11 );
 
 		       $scope.delete = function( flux ) {
 			   flux.$delete();
+			   $scope.current_flux = _($scope.current_flux).difference( [ flux ] );
+		       };
+
+		       $scope.save = function( flux ) {
+			   _(flux).has( 'id' ) ? flux.$update() : flux.$save();
+		       };
+
+		       $scope.dirtify = function( flux ) {
+			   flux.dirty = true;
 		       };
 
 		       $scope.add_flux = function() {
@@ -19,21 +36,22 @@ angular.module( 'portailApp' )
 			   } ) );
 		       };
 
-		       $scope.cancel = function () {
+		       $scope.close = function () {
 			   $modalInstance.close();
 		       };
 
-		       $scope.ok = function () {
-			   _($scope.current_flux).each( function( flux ) {
-			       if ( _(flux).has( 'id' ) ) {
-				   flux.$update();
-			       } {
-				   // le flux de notification de l'utilisateur a un champs titre
-				   if ( !_(flux).has( 'titre' ) && !_(flux.flux).isEmpty() && !_(flux.title).isEmpty() ) {
-				       flux.$save();
-				   }
-			       }
-			   } );
-			   $modalInstance.close();
-		       };
+		       // $scope.ok = function () {
+		       //	   _($scope.current_flux).each( function( flux ) {
+		       //	       console.log(flux)
+		       //	       if ( _(flux).has( 'id' ) ) {
+		       //		   flux.$update();
+		       //	       } {
+		       //		   // le flux de notification de l'utilisateur a un champs titre
+		       //		   if ( !_(flux).has( 'titre' ) && !_(flux.flux).isEmpty() && !_(flux.title).isEmpty() ) {
+		       //		       flux.$save();
+		       //		   }
+		       //	       }
+		       //	   } );
+		       //	   $modalInstance.close();
+		       // };
 		   } ] );
