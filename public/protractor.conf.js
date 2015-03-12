461f90c22a86e25262c251148fa1a63ccf37c47a
Gwenhael Le Moine <cycojesus@timac.erasme.org>
2015-03-12 09:40:04 +0100

    removed kludge
    Signed-off-by: Gwenhael Le Moine <cycojesus@timac.erasme.org>

--- public/protractor.conf.js
+++
@@ -1,6 +0,0 @@
-// see reference @ https://github.com/angular/protractor/blob/master/docs/referenceConf.js
-exports.config = {
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
-    chromeOnly: true,
-    specs: ['spec/**/*.js']
-};
