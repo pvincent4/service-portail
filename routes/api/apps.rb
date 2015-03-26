d86009ca70c6f06d30edddfe08a9fd0b7d995c54
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-03-26 16:42:14 +0100

    ugly hack over annuaire lack

--- routes/api/apps.rb
+++ routes/api/apps.rb
@@ -53,6 +53,9 @@ module Portail
                 application[ 'index' ] = default[ :index ] if application[ 'index' ] == -1
               end
 
+              # FIXME: if only there was a way to fix this in the Annuaire's DB
+              application[ 'icon' ].gsub!( 'charte-graphique-laclasse-com', 'laclasse-common-client' ) unless application[ 'icon' ].nil?
+
               application
             end
 
