f8ff85f9d7fe4c410dfc1b54a3c5dfefac963600
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-07-21 10:15:48 +0200

    removed debug message

--- routes/index.rb
+++ routes/index.rb
@@ -5,7 +5,6 @@ module Portail
     module Index
       def self.registered( app )
         app.get "#{APP_PATH}/?" do
-          LOGGER.debug( logged? ? 'utilisateur authentifié' : 'utilisateur anonyme' )
           if logged?
             erb :app
           else
