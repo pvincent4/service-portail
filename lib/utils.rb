8fd6664b237cde25341c8ab5ea913da68036101c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-08-28 09:13:10 +0200

    URI escaping is not my job, fixes #114

--- lib/utils.rb
+++ lib/utils.rb
@@ -2,12 +2,10 @@
 
 def normalize( params )
   params.each do |key, _value|
-    if params[ key ].is_a? String
-      params[ key ] = URI.escape( params[ key ] )
-    elsif params[ key ].is_a? Date
-      params[ key ] = URI.escape( params[ key ].iso8601 )
+    if params[ key ].is_a? Date
+      params[ key ] = params[ key ].iso8601
     else
-      params[ key ] = URI.escape( params[ key ].to_s )
+      params[ key ] = params[ key ].to_s
     end
   end
 end
