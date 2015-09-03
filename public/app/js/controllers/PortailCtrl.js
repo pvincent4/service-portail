d6c7eda312266c41f83eb5c2f531cb014465d4c8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-09-03 14:05:57 +0200

    back to angular 1.3.x

--- public/app/js/controllers/PortailCtrl.js
+++ public/app/js/controllers/PortailCtrl.js
@@ -14,35 +14,35 @@ angular.module( 'portailApp' )
 			   $state.go( 'portail.logged' );
 		       };
 
-		       $scope.carousel_index = 0;
-
 		       // TODO : faire une factory et un service pour les annonces.
 		       // L'idée est d'aller lire le flux twitter @laclasse avec le hash #sys
 		       $scope.annonce = ""; //"En moment sur Laclasse.com : La version 3 sort des cartons !";
+		       $scope.newsfeed = [];
 
-		       if ( $scope.current_user.is_logged ) {
-			   var retrieve_news = function() {
-			       news.get().then( function( response ) {
+		       var retrieve_news = function() {
+			   news.get()
+			       .then( function( response ) {
 				   $scope.newsfeed = _(response.data).map( function( item, index ) {
 				       item.id = index;
 				       item.trusted_description = $sce.trustAsHtml( item.description );
 
 				       return item;
 				   });
-				   $scope.news_index = 0;
+
+				   $scope.carouselIndex = 0;
 			       });
-			   };
+		       };
 
-			   $scope.config_news_fluxes = function() {
-			       $modal.open( {
-				   templateUrl: 'views/popup_config_news_fluxes.html',
-				   controller: 'PopupConfigNewsFluxesCtrl'
-			       } )
-				   .result.then( function() {
-				       retrieve_news();
-				   } );
-			   };
+		       $scope.config_news_fluxes = function() {
+			   $modal.open( {
+			       templateUrl: 'views/popup_config_news_fluxes.html',
+			       controller: 'PopupConfigNewsFluxesCtrl'
+			   } )
+			       .result.then( function() {
+				   retrieve_news();
+			       } );
+		       };
 
-			   retrieve_news();
-		       }
-		   } ] );
+		       retrieve_news();
+		   }
+		 ] );
